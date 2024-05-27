0d6448f355dd4d3d23ac5783fe0f76f6b90c1740
Uday <udayjoshi101292@gmail.com>
2024-05-28 03:52:37 +0530

    Updated DB Config

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-
-define('ROOT_URL', 'https://www.nuitsolutions.co.in/yaara');
+error_reporting(E_ALL);
+define('ROOT_URL', 'https://beingdigitalz.co.in/demo/yaara');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'u595440997_yaara');
+define('DB_USER', 'beingj4c_yaara');
 define('DB_PASS', 'T3qDdLB[9T!l');
-define('DB_NAME', 'u595440997_yaara');
+define('DB_NAME', 'beingj4c_yaara');
 
 //Site Url
 $site_url = ROOT_URL;  
