36d6f7879d50c8d64a0a70c2f46915d616cb4da7
Uday <udayjoshi101292@gmail.com>
2024-12-24 14:44:13 +0530

    Clearing the Files

--- data.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-//Car Brands
-$yc_brands_table = 'SELECT * FROM yc_uae_master';
-
-$yc_brands_data = mysqli_query($conn, $yc_brands_table);
-
-//Car Brand Results
-$yc_brands = mysqli_fetch_all($yc_brands_data, MYSQLI_ASSOC);
--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Title to Slug
-function url_slug($value){
-   
-    $slug = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $value)));
-
-    return rtrim($slug, '-');
-}
